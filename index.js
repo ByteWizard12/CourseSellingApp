e2f8c7d8049a8561bce194e78dbb9294aed2c474
bytewizard12 <harshyadav12012004@gmail.com>
2025-02-14 21:20:57 +0530

    Added skeleton routes for the CourseSellingApp

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = required("express");
+const app = express();
+
+
+app.post("/user/signup" , async( req , res) => {
+    res.json({
+        message : "signupendpoint"
+    });
+});
+app.post("/user/signin" , async( req , res) => {
+    res.json({
+        message : "signinendpoint"
+    });
+});
+app.post("/user/purchase" , async( req , res) => {
+
+});
+
+app.get("/course/purchases" , async( req , res) => {
+
+});
+
+app.get("/courses" , async( req , res) => {
+
+});
+
+
+
+app.listen(3000 , () => {
+    console.log("Server is running on the port : http://localhost:3000" );
+});
