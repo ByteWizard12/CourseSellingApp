6a6075b65026dfe298df05b417b1a097ff026fcf
bytewizard12 <harshyadav12012004@gmail.com>
2025-02-15 00:40:36 +0530

    Updated course.js by removing /course in course.js

--- routes/course.js
+++ routes/course.js
@@ -1,11 +1,11 @@
 const {Router} = require("express");
 const courseRouter = Router();
 
-courseRouter.get("/course/purchase" , async( req , res) => {
+courseRouter.get("/purchase" , async( req , res) => {
 
 });
 
-courseRouter.get("/course/preview" , async( req , res) => {
+courseRouter.get("/preview" , async( req , res) => {
 
 });
 
