76cfdcc8052bb044563261494322593ea73694dd
bytewizard12 <harshyadav12012004@gmail.com>
2025-02-19 15:21:09 +0530

    Added authorization end points

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,49 @@
 const {Router} = require("express");
 const adminRouter = Router();
 const {adminModel} = require("../db");
+const jwt = require("jsonwebtoken");
+const JWT_ADMIN_PASSWORD = "iamadmin";
 
 
 adminRouter.post("/signup" , async( req , res) => {
+    const {email , password , firstName  , lastName } = req.body; 
+    // TODO : adding zod validation and hash the password so plain text is not stored in the DB , put inside a try catch block
     
+    await adminModel.create({
+        email,
+        password,
+        firstName,
+        lastName
+    })
+
+    res.json({
+        message  : "Signup succeeded"
+    })
 });
 
 adminRouter.post("/signin" , async( req , res) => {
-    res.json({
-        message : "signinendpoint"
+    const {email , password} = req.body;
+
+    // TODO : Password -> hashed 
+    const admin = await adminModel.findOne({
+        email : email,
+        password : password
     });
+
+    if(admin){
+        const token = jwt.sign({
+            id : admin._id
+        } , JWT_ADMIN_PASSWORD);
+
+        res.json({
+            token : token
+        })
+    }
+    else{
+        res.status(403).json({
+            message : "Incorrect Credentials"
+        })
+    }
 });
 
 adminRouter.post("/course" , async(req , res) => {
@@ -25,4 +58,4 @@ adminRouter.get("/course/bulk" , async(req , res) => {
 
 module.exports = {
     adminRouter : adminRouter
-}
+} 
--- routes/user.js
+++ routes/user.js
@@ -1,18 +1,48 @@
 const {Router } = require("express");
 const userRouter = Router();
 const {userModel} = require("../db");
-
+const jwt = require("jsonwebtoken");
+const JWT_USER_PASSWORD = "iamuser";
 
 userRouter.post("/signup" , async( req , res) => {
+    const {email , password , firstName  , lastName } = req.body; 
+    // TODO : adding zod validation and hash the password so plain text is not stored in the DB , put inside a try catch block
+    
+    await userModel.create({
+        email,
+        password,
+        firstName,
+        lastName
+    })
+
     res.json({
-        message :"signupendpoint"
-    });
+        message  : "Signup succeeded"
+    })
 });
 
 userRouter.post("/signin" , async( req , res) => {
-    res.json({
-        message : "signinendpoint"
+    const {email , password} = req.body;
+
+    // TODO : Password -> hashed 
+    const user = await userModel.findOne({
+        email : email,
+        password : password
     });
+
+    if(user){
+        const token = jwt.sign({
+            id : user._id
+        } , JWT_USER_PASSWORD);
+
+        res.json({
+            token : token
+        })
+    }
+    else{
+        res.status(403).json({
+            message : "Incorrect Credentials"
+        })
+    }
 });
 
 userRouter.post("/purchase" , async( req , res) => {
